f816d2915918f4d4f4e2549656d29aa4a966d566
luciaaroca <lucia.aroca2510@gmail.com>
2025-12-16 16:14:48 +0100

    graficos MKT

--- frontend/src/components/Main/MktPage/MktPage.jsx
+++ frontend/src/components/Main/MktPage/MktPage.jsx
@@ -1,4 +1,6 @@
+import React,{useState } from "react";
 import SalesList from "../../Main/MktPage/SalesList/SalesList";
+import SalesChart from "../../Main/MktPage/SalesChart/SalesChart";
 import CustomersList from "../../Main/MktPage/CustomersList/CustomersList";
 import ProductsList from "../../Main/MktPage/ProductsList/ProductsList";
 import "./MktPage.css";
@@ -6,13 +8,24 @@ import ChatBox from "../../Chat/Chatbox/Chatbox";
 
 
 const MktPage = () => {
+  const [showSalesList, setShowSalesList] = useState(false); // controla qué mostrar SalesList/SalesChart
+  
   return (
     <section className="mkt-page">
       <header className="mkt-header">
         <h1>📊 Marketing Dashboard</h1>
+        
       </header>
 
+      {/* Sección de ventas */}
+     {showSalesList ? (
       <SalesList />
+        ) : (
+      <SalesChart
+          onViewSalesList={() => setShowSalesList(true)}
+         showViewTableButton={true}
+      />
+)}
       <CustomersList />
       <ProductsList />
       <ChatBox />
